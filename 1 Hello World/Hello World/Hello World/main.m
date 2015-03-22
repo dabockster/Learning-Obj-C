19d14aeceaa04e6fbb480def3e919af8300db02c
Steven Bock <steven.bockster@gmail.com>
2015-03-22 00:04:34 -0700

    Added Hello World code

---
+++ 1 Hello World/Hello World/Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Steven Bock on 3/22/15.
+//  Copyright (c) 2015 Steven Bock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
